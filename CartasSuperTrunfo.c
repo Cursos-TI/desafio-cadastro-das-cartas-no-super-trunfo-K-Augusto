392ba09b0f0568074dc3868c57870c44d465ee7e
K-Augusto <202502643021@alunos.estacio.br>
2025-03-02 00:53:26 +0000

    Alteracao e teste do codigo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,25 +21,30 @@ int main() {
     //Variáveis declaradas
     char EstadoAlagoas = 'A',EstadoAmazonas ='B';
     char CodigoCarta1[4] = "A01", CodigoCarta2[4] = "B02";
-    char NomeCidade1[50] = "Maceió", NomeCidade2[50] = "Manaus";
+    char NomeCidade1[50] = "MACEIO", NomeCidade2[50] = "MANAUS";
     int PopulacaoMaceio = 957916, PopulacaoManaus = 2279686;
     float AreaemKmMaceio = 509320.00, AreaemKmManaus = 11401.092;
-    float PibMaceio = 27484016033, PibManaus = 103281430065;
+    float PibMaceio = 27484016.31, PibManaus = 103281436.041;
     int PontTuristMaceio = 30, PontTuristManaus = 12;
     
-    printf("\nDigite a letra que representa o estado.: ");
+    //Instruções ao usuário
+    printf("Atenção usuário, utilize letra maiúscula e ponto como separador de dezenas!!!\n");
+
+    //Solicitando ao 1º jogador para digitar as informações da carta 1
+    printf("\nJogador número 1, digite as informações da carta 1:\n");
+    printf("\nDigite a letra que representa o Estado: ");
     scanf(" %c",&EstadoAlagoas);
-    printf("Digite o código da carta.: ");
+    printf("Digite o código da carta: ");
     scanf("%s",CodigoCarta1);
     printf("Digite nome da cidade: ");
     scanf("%s",NomeCidade1);
     printf("Digite o tamanho da população: ");
     scanf("%d",&PopulacaoMaceio);
-    printf("Digite o tamanho do território:");
+    printf("Digite o o tamanho do território: ");
     scanf("%f",&AreaemKmMaceio);
     printf("Digite o Pib da cidade: ");
     scanf("%f",&PibMaceio);
-    printf("Digite a quntidade de pontos turísticos: ");
+    printf("Digite a quantidade de pontos turísticos: ");
     scanf("%d",&PontTuristMaceio);
 
     //Impressão da 1ª carta registrada
@@ -49,22 +54,24 @@ int main() {
     printf("Nome da cidade: %s\n",NomeCidade1);
     printf("População: %d\n",PopulacaoMaceio);
     printf("Área: %.2f Km²\n",AreaemKmMaceio);
-    printf("PIB: %.2f milhões de reais\n",PibMaceio);
+    printf("PIB: %.2f milhões de reais\n",PibMaceio/1000000);//Transformando reais para milhões de reais
     printf("Número de pontos turísticos: %d\n",PontTuristMaceio);
 
-    printf("\nDigite a letra que representa o estado.: ");
+    //Solicitando ao 2º jogador para digitar as informações da carta 2
+    printf("\nJogador número 2, digite as informações da carta 2:\n");
+    printf("\nDigite a letra que representa o Estado: ");
     scanf(" %c",&EstadoAmazonas);
-    printf("Digite o código da carta.: ");
+    printf("Digite o código da carta: ");
     scanf("%s",CodigoCarta2);
     printf("Digite nome da cidade: ");
     scanf("%s",NomeCidade2);
     printf("Digite o tamanho da população: ");
     scanf("%d",&PopulacaoManaus);
-    printf("Digite o tamanho do território:");
+    printf("Digite o tamanho do território: ");
     scanf("%f",&AreaemKmManaus);
     printf("Digite o Pib da cidade: ");
     scanf("%f",&PibManaus);
-    printf("Digite a quntidade de pontos turísticos: ");
+    printf("Digite a quantidade de pontos turísticos: ");
     scanf("%d",&PontTuristManaus);
 
     //Impressão da 2ª carta registrada
@@ -74,7 +81,7 @@ int main() {
     printf("Nome da cidade: %s\n",NomeCidade2);
     printf("População: %d\n",PopulacaoManaus);
     printf("Área: %.2f Km²\n",AreaemKmManaus);
-    printf("PIB: %.2f milhões de reais\n",PibManaus);
+    printf("PIB: %.2f milhões de reais\n",PibManaus/1000000);//Transformando reais para milhões de reais
     printf("Número de pontos turísticos: %d\n",PontTuristManaus);
 
     return 0;
